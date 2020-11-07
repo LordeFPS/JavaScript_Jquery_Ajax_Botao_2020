5f5f1c336fc63d3da78ebe5f3fa3346b7c263ed8
lordefps <lodizinho_mice@hotmail.com>
2020-11-07 16:24:36 -0300

    Laços

--- Aula1_WebApplication/web/desvio.js
+++ Aula1_WebApplication/web/desvio.js
@@ -17,8 +17,8 @@ document.write("<br /> <br />DESVIOO");
  */
 
 
-var valor1 = 5;
-var valor2 = 10;
+var valor1 = 10;
+var valor2 = 5;
 
 document.write("<br /> <br />Valor 1: " + valor1);
 document.write("<br />Valor 2: " + valor2);
@@ -46,11 +46,11 @@ document.write("<br />Diferente !== : ");
 document.write(valor1 !== valor2);
 
 document.write("<hr />");
-document.write("<br /> Usando IF");
+document.write("<br /> Usando IF <br />");
 
 if(valor1 > valor2){
     
-    document.write("<br /> <br />Valor1: " + valor1 + " é maior que Valor2: " + valor2);
+    document.write("<br /> Valor1: " + valor1 + " é maior que Valor2: " + valor2);
 }else{
     document.write("<br /> Valor1: " + valor1 + " é menor que Valor2: " + valor2);
 }
