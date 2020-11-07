2f489e803b1aab94902f65afe846c087f9e930ec
lordefps <lodizinho_mice@hotmail.com>
2020-11-07 15:12:52 -0300

    JavaScript_Tomcat

---
+++ Aula1_WebApplication/web/arquivo1.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    var nome;
+            
+    nome = "Jose da Silva"; 
+            
+    document.write("Oi pessoas do SCRIPT!!");
+    document.write("<br /> Seu nome é " + nome);
+
+    var valor1 = 10;
+    var valor2 = 5;
+   
+    /*
+       + soma
+       - subtração
+       / divisão
+       * multiplicação
+       % resto de divisão
+    */
+   
+    document.write("<br /> <br />Valor 1: " + valor1);
+    document.write("<br />Valor 2: " + valor2);
+
+    document.write("<br /><br />Soma: ");
+    document.write(valor1 + valor2);
+
+    document.write("<br /> Subtração: ");
+    document.write(valor1 - valor2);
+
+    document.write("<br /> Multiplicação: ");
+    document.write(valor1 * valor2);
+
+    document.write("<br /> Divisão: ");
+    document.write(valor1 / valor2);
+
+    document.write("<br /> Resto: ");
+    document.write(valor1 % valor2);
+
